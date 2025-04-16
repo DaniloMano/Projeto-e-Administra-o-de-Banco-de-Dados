e0850c7f42c3773f41c68217537264d75d5ad850
danilo <danilogabrill@outlook.com>
2025-04-15 22:57:27 -0300

    docs: tarefa01-q14 feito issue#1

--- tarefas/t01/consultas/tarefa01-q14.sql
+++ tarefas/t01/consultas/tarefa01-q14.sql
@@ -1,2 +1,13 @@
 -- 14-Faça uma consulta que selecione o nome dos funcionários responsáveis por atividades,
 -- o número de atividades que este funcionário é responsável. Crie e use views na consulta.
+
+create view atividades_por_funcionario as
+select f.nome, COUNT(a.codigo) AS num_atividades
+from funcionario f
+left join atividade a ON f.codigo = a.cod_responsavel
+group by f.codigo, f.nome;
+
+select nome, num_atividades
+from atividades_por_funcionario;
+
+-- usando left join a query mostra até quem não é responsável por nenhuma atividade.
